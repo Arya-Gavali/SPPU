6e9c5d174f34f274a7c7bac146b87b6f3639363b
AryaGavali04 <varadgavali8292@gmail.com>
2025-07-28 11:35:04 +0530

    My First Push

---
+++ Add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+
+{
+    int a=5;
+    int b=7;
+
+    printf("%d is the sum ",a+b);
+
+    return 0;
+
+}
---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ 
+int main(){
+    printf("hi varad...!");
+     printf(":)");
+    return 0;
+}
+
